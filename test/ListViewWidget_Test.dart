b569a869b6908b22c25a411a0d995ba0befbd819
Noah <noah.nayagam@mcruncher.com>
2024-04-01 08:42:55 +0800

    #1 Added a simple widget test case to check the list item length

--- test/ListViewWidget_Test.dart
+++ test/ListViewWidget_Test.dart
@@ -5,6 +5,11 @@ import 'package:worshipsongs_app/main.dart';
 
 
 void main() {
+  setUpAll(() => null);
+  setUp(() => null);
+  tearDown(() => null);
+  tearDownAll(() => null);
+  
   List<Map> songs = [{1: new Song(id: 1, title: "Foo")}];
 
   testWidgets('Listview items count test', (WidgetTester tester) async {
@@ -12,5 +17,19 @@ void main() {
     await tester.pumpWidget(MyApp(songs: songs));
 
     expect(1, songs.length);
+  });
+
+  testWidgets('Listview item - id test', (WidgetTester tester) async {
+
+    await tester.pumpWidget(MyApp(songs: songs));
+
+    expect(1, songs[0].keys.first);
+  });
+
+  testWidgets('Listview item - comment test', (WidgetTester tester) async {
+
+    await tester.pumpWidget(MyApp(songs: songs));
+
+    expect('Foo', songs[0].values.first);
   });
 }
