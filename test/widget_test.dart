815cbfdb01df9585993221c5e39d06c562c6e4dc
Noah <noah.nayagam@mcruncher.com>
2024-04-12 11:27:19 +0800

    #1 Updated the tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,17 @@
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:worshipsongs_app/domain/Song.dart';
-import 'package:worshipsongs_app/widgets/ListViewWidget.dart';
 
 void main() {
   List<Song> songs = [Song(id: 1, title: "Seekiramaai Vanthiduvaen Endru Sonneerae", lyrics: "test lyrics")];
   
   test('Get song title', ()
   {
-    expect(ListViewWidget().getListTitle(songs, 0).title, "Seekiramaai Vanthiduvaen Endru Sonneerae");
+    expect(songs[0].title, "Seekiramaai Vanthiduvaen Endru Sonneerae");
+  });
+
+  test('Get song lyrics', ()
+  {
+    expect(songs[0].lyrics, "test lyrics");
   });
 }
