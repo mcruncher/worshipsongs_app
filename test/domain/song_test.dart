012c55ea15615c4f03d17707cd7d562af3d0f3b5
Noah <noah.nayagam@mcruncher.com>
2024-05-08 08:35:58 +0800

    #WA-2 Moved domain test into respective test classes

--- test/domain/song_test.dart
+++ test/domain/song_test.dart
@@ -1,20 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:worshipsongs_app/domain/Author.dart';
-import 'package:worshipsongs_app/domain/Song.dart';
 
 void main() {
-  List<Song> songs = [Song(id: 1, title: "Seekiramaai Vanthiduvaen Endru Sonneerae", lyrics: "test lyrics")];
   List<Author> authors = [Author(name: "Foo Author", songs: 450, tamilName: "", defaultName: "Foo Author"), Author(name: "Bar Author", songs: 58, tamilName: "", defaultName: "Bar Author"), Author(name: "Arulanantham {அருளானந்தம்}", songs: 152, tamilName: "அருளானந்தம்", defaultName: "Arulanantham ")];
-  
-  test('Get song title', ()
-  {
-    expect(songs[0].title, "Seekiramaai Vanthiduvaen Endru Sonneerae");
-  });
-
-  test('Get song lyrics', ()
-  {
-    expect(songs[0].lyrics, "test lyrics");
-  });
 
   test('Get author name', ()
   {
